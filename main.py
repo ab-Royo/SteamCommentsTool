389dede17c9c6532ed7506a06718cb8c66d0f058
abRoy <abroyo@outlook.com>
2023-08-02 12:34:56 +0800

    feat!: 使用SteamDB数据库，使用recently判断是否发送

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ import Settings
 import Logcolor
 from utils import DataBase, Update
 # 项目版本
-__version__ = "0.3.0"
+__version__ = "1.0.0-alpha"
 # 定义成功和失败的次数
 success_count = 0
 fail_count = 0
+not_send_count = 0
 
 # 版本信息，检查更新
 print(Logcolor.responseINFO() + "SteamCommentsTool 当前版本 v" + __version__)
 Update.checkUpdate()
 # 检测数据库存在, 初始化
-DataBase.initDB()
+# DataBase.initDB()
+DataBase.initSteamDB()
 
 # 创建 Steam 客户端对象
 client = steam.SteamClient()
@@ -44,7 +46,8 @@ else:
     print(Logcolor.responseWARN() + "代理模式未启用")
 
 # 连接数据库，获取 64 位 ID 和昵称
-friends = DataBase.getDatabase()
+# friends = DataBase.getDatabase()
+friends = DataBase.getSteamDB()
 
 # 手动输入留言文本，使用 {0} 作为昵称的占位符
 # TODO：json留言支持
@@ -56,7 +59,7 @@ text = text.replace("{n}", "\n")
 # 遍历好友列表，替换昵称，发送留言请求
 if Enable:
     proxy = {ProxyMode: ProxyURL}  # 拼接 proxy
-    for id, nickname in friends:
+    for userID, nickname, recently in friends:
         # 替换昵称
         message = text.format(nickname)
         # 构造留言请求的 URL 和参数
@@ -71,37 +74,41 @@ if Enable:
             "sessionid": cookie_dict["sessionid"],
             "feature2": -1
         }
-        # 发送留言请求
-        try:
-            response = requests.post(url, headers=Headers, data=data, proxies=proxy, cookies=cookie_dict)
-            # 检查留言是否成功
-            if response.json()["success"]:
-                print(Logcolor.responseINFO() + f" {nickname} 留言成功！https://steamcommunity.com/profiles/{id}")
-                # 更新成功次数
-                success_count += 1
-            else:
-                print(Logcolor.responseERROR() + f" {nickname} 留言失败！https://steamcommunity.com/profiles/{id}")
-                print(Logcolor.responseWARN() + f"{nickname} POST错误信息为：{response.json()}")
+        if recently == "true":
+            # 发送留言请求
+            try:
+                response = requests.post(url, headers=Headers, data=data, proxies=proxy, cookies=cookie_dict)
+                # 检查留言是否成功
+                if response.json()["success"]:
+                    print(Logcolor.responseINFO() + f" {nickname} 留言成功！https://steamcommunity.com/profiles/{id}")
+                    # 更新成功次数
+                    success_count += 1
+                else:
+                    print(Logcolor.responseERROR() + f" {nickname} 留言失败！https://steamcommunity.com/profiles/{id}")
+                    print(Logcolor.responseWARN() + f"{nickname} POST错误信息为：{response.json()}")
+                    # 更新失败次数
+                    fail_count += 1
+            except Exception as e:
+                print(Logcolor.responseERROR() + f"requests异常：{e}")
                 # 更新失败次数
                 fail_count += 1
-        except Exception as e:
-            print(Logcolor.responseERROR() + f"requests异常：{e}")
-            # 更新失败次数
-            fail_count += 1
-        # 随机间隔1-3秒，减少风控概率
-        time.sleep(random.uniform(1, 3))
+            # 随机间隔2-5秒，减少风控概率
+            time.sleep(random.uniform(2, 5))
+        else:
+            print(Logcolor.responseWARN() + f" {nickname} 不发送留言，recently值为false")
+            not_send_count += 1
     # 打印成功和失败的次数
-    print(Logcolor.responseINFO() + f"完成全部留言，成功 {success_count} 次，失败 {fail_count} 次。")
+    print(Logcolor.responseINFO() + f"完成全部留言，成功 {success_count} 次，失败 {fail_count} 次，不发送{not_send_count} 次。")
 
 # 如果代理未开启
 else:
-    for id, nickname in friends:
+    for userID, nickname, recently in friends:
         # 替换昵称
         message = text.format(nickname)
         # 构造留言请求的 URL 和参数
         Headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/114.0.0.0 Safari/537.36'
+                          'Chrome/110.0.0.0 Safari/537.36'
         }
         url = f"https://steamcommunity.com/comment/Profile/post/{id}/-1/"
         data = {
@@ -110,24 +117,28 @@ else:
             "sessionid": cookie_dict["sessionid"],
             "feature2": -1
         }
-        # 发送留言请求
-        try:
-            response = requests.post(url, headers=Headers, data=data, cookies=cookie_dict)
-            # 检查留言是否成功
-            if response.json()["success"]:
-                print(Logcolor.responseINFO() + f" {nickname} 留言成功！https://steamcommunity.com/profiles/{id}")
-                # 更新成功次数
-                success_count += 1
-            else:
-                print(Logcolor.responseERROR() + f" {nickname} 留言失败！https://steamcommunity.com/profiles/{id}")
-                print(Logcolor.responseWARN() + f"{nickname} POST错误信息为：{response.json()}")
+        if recently == "True":
+            # 发送留言请求
+            try:
+                response = requests.post(url, headers=Headers, data=data, cookies=cookie_dict)
+                # 检查留言是否成功
+                if response.json()["success"]:
+                    print(Logcolor.responseINFO() + f" {nickname} 留言成功！https://steamcommunity.com/profiles/{id}")
+                    # 更新成功次数
+                    success_count += 1
+                else:
+                    print(Logcolor.responseERROR() + f" {nickname} 留言失败！https://steamcommunity.com/profiles/{id}")
+                    print(Logcolor.responseWARN() + f"{nickname} POST错误信息为：{response.json()}")
+                    # 更新失败次数
+                    fail_count += 1
+            except Exception as e:
+                print(Logcolor.responseERROR() + f"requests异常：{e}")
                 # 更新失败次数
                 fail_count += 1
-        except Exception as e:
-            print(Logcolor.responseERROR() + f"requests异常：{e}")
-            # 更新失败次数
-            fail_count += 1
-            # 随机间隔1-3秒，减少风控概率
-            time.sleep(random.uniform(1, 3))
+            # 随机间隔2-5秒，减少风控概率
+            time.sleep(random.uniform(2, 5))
+        else:
+            print(Logcolor.responseWARN() + f" {nickname} 不发送留言，recently值为false")
+            not_send_count += 1
     # 打印成功和失败的次数
-    print(Logcolor.responseINFO() + f"完成全部留言，成功 {success_count} 次，失败 {fail_count} 次。")
+    print(Logcolor.responseINFO() + f"完成全部留言，成功 {success_count} 次，失败 {fail_count} 次，不发送{not_send_count} 次。")
